421a63bd136516734519c6931855daa7fb42c023
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-05-11 11:43:39 +0530

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./aria.sh;
-git clone https://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@github.com/gillzxo/gdtot-Slam-mirror ; cd gdtot-Slam-mirror
+git clone https://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@github.com/gillzxo/temp-mirr ; cd temp-mirr
 
 
 python3 -m bot
