dc776949bd562885a16590980b187a1f099ea140
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-05-12 09:03:20 +0530

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -2,8 +2,8 @@ apt update && apt upgrade -y
 apt install git -y
 _other_installs
 pip3 install -U pip
-git clone https://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@github.com/gillzxo/Testapp
+git clone https://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@github.com/gillzxo/Testapp
 cd Testapp
 pip3 install -U -r requirements.txt
 _externals
-python main.py
+python3 main.py
