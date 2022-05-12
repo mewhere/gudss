f22bac8e94c3c32a4030f717851bf550ce429534
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-05-12 09:10:34 +0530

    Delete mirror.sh

--- mirror.sh
+++
@@ -1,9 +0,0 @@
-apt update && apt upgrade -y
-apt install git -y
-_other_installs
-pip3 install -U pip
-git clone https://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@github.com/gillzxo/Testapp
-cd Testapp
-pip3 install -U -r requirements.txt
-_externals
-python3 -m bot
