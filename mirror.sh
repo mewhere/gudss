f377a2f4669d70c87fc3fba6ab842aed8fdbbe15
mewhere <83624712+mewhere@users.noreply.github.com>
2022-05-14 10:46:00 +0530

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,3 +1,18 @@
-git clone $REPO_URL Github_Repo
-cd Github_Repo
-python3 $PYTHON_FILE
+_start () {
+    local zippath
+    zippath="Testapp.zip"
+    echo "••• DOWNLOADING v3 •••"
+    wget -q $(_repo_zip) -O "$zippath"
+    ZIP_PATH=$(zipinfo -1 "$zippath" | grep -v "/.");
+    unzip -qq "$zippath"
+    rm -rf "$zippath"
+    _run_deploy
+    cd $ZIP_PATH
+    _deploy_repo
+    python3 ../setup/updater.py ../requirements.txt requirements.txt
+    chmod -R 755 bin
+
+    echo "••• DOWNLOAD COMPLETE •••"
+
+    python3 $Python_file
+}
